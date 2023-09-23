5de51c7ecc4210cc7fb84c5f190a2ddd220b82b6
Maria Mendonca <maria.mendonca156@gmail.com>
2023-09-23 18:37:49 +0530

    Added color gradient, buttons for direction

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+const height = window.innerHeight;
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [col1, setCol1] = useState('#d9a7c7');
+  const [col2, setCol2] = useState('#fffcdc');
+  const [direction, setDirection] = useState('right');
   return (
-    <>
+    <div
+      style={{
+        height: height,
+        backgroundImage: `linear-gradient(to ${direction}, ${col1}, ${col2})`,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <h1 style={{ fontSize: '3rem' }}>Gradient Color Selector</h1>
+      <p>Choose Direction</p>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button>Top</button>
+        <button>Right</button>
+        <button>Bottom</button>
+        <button>Left</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div style={{ display: 'flex', gap: '2rem' }}>
+        <input
+          type="color"
+          value={col1}
+          onChange={(e) => setCol1(e.target.value)}
+          style={InputStyles}
+        />
+        <input
+          type="color"
+          value={col2}
+          onChange={(e) => setCol2(e.target.value)}
+          style={InputStyles}
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+// Styles
+const InputStyles = {
+  width: '100px',
+  height: '50px',
+  border: 'none',
+  padding: '0',
+  backgroundColor: 'transparent',
+};

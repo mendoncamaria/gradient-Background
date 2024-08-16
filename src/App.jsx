96fe2dc5c02a74d2312aed46793a1edaac6bbdfd
Maria Mendonca <maria.mendonca156@gmail.com>
2024-08-16 08:35:41 +0530

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import {
   Button,
   ButtonContainer,
+  Footer,
+  FooterLink,
   Heading,
   InputContainer,
   InputStyles,
@@ -15,63 +17,74 @@ function App() {
   const [gradientDirection, setGradientDirection] = useState('right');
 
   return (
-    <MainContainer
-      style={{
-        backgroundImage: `linear-gradient(to ${gradientDirection}, ${color1}, ${color2})`,
-      }}
-    >
-      <Heading>Gradient Color Selector</Heading>
+    <>
+      <MainContainer
+        style={{
+          backgroundImage: `linear-gradient(to ${gradientDirection}, ${color1}, ${color2})`,
+        }}
+      >
+        <Heading>Gradient Color Selector</Heading>
 
-      <ButtonContainer>
-        <Button
-          onClick={() => setGradientDirection('top')}
-          isSelected={gradientDirection === 'top'}
+        <ButtonContainer>
+          <Button
+            onClick={() => setGradientDirection('top')}
+            isSelected={gradientDirection === 'top'}
+          >
+            Top
+          </Button>
+
+          <Button
+            onClick={() => setGradientDirection('right')}
+            isSelected={gradientDirection === 'right'}
+          >
+            Right
+          </Button>
+
+          <Button
+            onClick={() => setGradientDirection('bottom')}
+            isSelected={gradientDirection === 'bottom'}
+          >
+            Bottom
+          </Button>
+
+          <Button
+            onClick={() => setGradientDirection('left')}
+            isSelected={gradientDirection === 'left'}
+          >
+            Left
+          </Button>
+        </ButtonContainer>
+
+        <InputContainer>
+          <InputSubContainer>
+            <InputStyles
+              type="color"
+              value={color1}
+              onChange={(e) => setColor1(e.target.value)}
+            />
+            {color1.toUpperCase()}
+          </InputSubContainer>
+
+          <InputSubContainer>
+            <InputStyles
+              type="color"
+              value={color2}
+              onChange={(e) => setColor2(e.target.value)}
+            />
+            {color2.toUpperCase()}
+          </InputSubContainer>
+        </InputContainer>
+      </MainContainer>
+      <Footer>
+        &copy; 2023{' '}
+        <FooterLink
+          href="https://maria-mendonca.vercel.app/"
+          target="_self"
         >
-          Top
-        </Button>
-
-        <Button
-          onClick={() => setGradientDirection('right')}
-          isSelected={gradientDirection === 'right'}
-        >
-          Right
-        </Button>
-
-        <Button
-          onClick={() => setGradientDirection('bottom')}
-          isSelected={gradientDirection === 'bottom'}
-        >
-          Bottom
-        </Button>
-
-        <Button
-          onClick={() => setGradientDirection('left')}
-          isSelected={gradientDirection === 'left'}
-        >
-          Left
-        </Button>
-      </ButtonContainer>
-
-      <InputContainer>
-        <InputSubContainer>
-          <InputStyles
-            type="color"
-            value={color1}
-            onChange={(e) => setColor1(e.target.value)}
-          />
-          {color1.toUpperCase()}
-        </InputSubContainer>
-
-        <InputSubContainer>
-          <InputStyles
-            type="color"
-            value={color2}
-            onChange={(e) => setColor2(e.target.value)}
-          />
-          {color2.toUpperCase()}
-        </InputSubContainer>
-      </InputContainer>
-    </MainContainer>
+          Maria Mendonca
+        </FooterLink>
+      </Footer>
+    </>
   );
 }
 
